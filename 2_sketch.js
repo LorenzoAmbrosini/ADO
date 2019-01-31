d2e6a680d9ef0d13fe90e5040ea00477c8be0815
Lorenzo Ambrosini <lorenzo.ambrosini@mail.polimi.it>
2019-01-31 17:12:17 +0100

    Ordine corretto

--- 2_sketch.js
+++ 2_sketch.js
@@ -1,82 +1,89 @@
-var myRec = new p5.SpeechRec(); // new P5.SpeechRec object
-var song;
-myRec.continuous = true; // do continuous recognition
+var capture;
+var sfondo;
+var data;
 
-var mySpeech = new p5.Speech();
-mySpeech.setLang('en-UK');
-
- // speech synthesis object
-
-
-
+var balls = [];
 
 function preload() {
- song = loadSound('./assets/2/speaking.mp3');
+  sfondo = loadImage("./assets/4/Departures.png");
+  data = loadJSON('./assets/4/data-24-10-2017.json');
+
 }
 
-function setup()
-{
-  // graphics stuff:
-
+function setup() {
   createCanvas(windowWidth, windowHeight);
+  capture = createCapture(VIDEO);
+  capture.size(640, 480);
+  capture.hide();
 
-  fill(0, 0, 0, 255);
+  //console.log(data.people.length);
+  for (var i = 0; i < 18; i++) {
 
-  textSize(32);
+    // properties
+    var x = 120;
+    var y = i * 30 + 150;
+    var k = round(random(0, 9))
+    var name = data.people[k].name;
 
-  myRec.onResult = showResult;
- mySpeech.setVolume(0.8);
+    var colorRandomizer = random(0.4, 1);
 
- mySpeech.listVoices();
-mySpeech.setVoice(3);
-mySpeech.setRate(0.8);
-mySpeech.setPitch(1);
-
-
-song.setVolume(0.3);
-
-
-
+    // create the ball object and add it to the array
+    var myBall = new Ball(x, y, colorRandomizer, name);
+    balls.push(myBall);
+  }
 }
 
-function draw()
-{
 
-    ellipse(50, 50, 80, 80);
-}
 
-function activate() {
+function draw() {
 
-}
+  background(0);
+  background(sfondo);
+  //imageMode(CENTER)
+  //image(sfondo, windowWidth/2, windowHeight/2, sfondo.width/2, sfondo.height/2);
 
-function showResult()
-{
 
-    background(192, 255, 192);
-if (myRec.onResult) {
-  textAlign(CENTER);
-    text(myRec.resultString, (width/2) - 300, (height/2) - 100 ,600,600)
+  for (var j = 0; j < balls.length; j++) {
+
+    var randomCorrect = random(0.4, 1);
+    balls[j].display(randomCorrect);
   }
 
 }
 
+
+
+function Ball(_x, _y, _colorRandomizer, _name) {
+
+  this.x = _x;
+  this.y = _y;
+
+
+  this.display = function(_randomCorrect) {
+
+    var c = capture.get(noise(capture.width / 2), noise(capture.height / 2));
+    var bright = _colorRandomizer * lightness(c);
+    var col = color(255, 255, 0, bright * 5);
+
+    textSize(20);
+    if (_name == "London") {
+
+      console.log(_randomCorrect);
+      bright = _randomCorrect * bright + 30;
+      col = color(255, 255, 0, bright * 10);
+      fill(col);
+      text(_name, this.x, this.y);
+    } else {
+      fill(col);
+      text(_name, this.x, this.y);
+    }
+
+  }
+}
+
+
 function mousePressed() {
-if ( song.isPlaying() ) { // .isPlaying() returns a boolean
-    song.stop();
-    background(255,0,0);
-  } else {
-
-    mySpeech.speak('Hey there! Where do you wanna go?');
-    myRec.start();
-    song.play();
-    background(0,255,0);
-
-
-      if ( mouseX <= 80 &&
-           mouseY <= 80){
-
-        window.open("3_intermezzo.html", "_self");
-      }
-
+  if (mouseX <= 80 && mouseY <= 80) {
+    window.open("3_intermezzo.html", "_self");
   }
 }
--- 4_sketch.js
+++ 4_sketch.js
@@ -1,82 +1,75 @@
-var capture;
-var sfondo;
-var data;
+var myRec = new p5.SpeechRec(); // new P5.SpeechRec object
+var song;
+myRec.continuous = true; // do continuous recognition
 
-var balls = [];
+var mySpeech = new p5.Speech();
+mySpeech.setLang('en-UK');
+
+ // speech synthesis object
+
+
+
 
 function preload() {
-  sfondo = loadImage("./assets/4/Departures.png");
-  data = loadJSON('./assets/4/data-24-10-2017.json');
-
+ song = loadSound('./assets/2/speaking.mp3');
 }
 
-function setup() {
+function setup()
+{
+  // graphics stuff:
+
   createCanvas(windowWidth, windowHeight);
-  capture = createCapture(VIDEO);
-  capture.size(640, 480);
-  capture.hide();
 
-  //console.log(data.people.length);
-  for (var i = 0; i < 18; i++) {
+  fill(0, 0, 0, 255);
 
-    // properties
-    var x = 120;
-    var y = i * 30 + 150;
-    var k = round(random(0, 9))
-    var name = data.people[k].name;
+  textSize(32);
 
-    var colorRandomizer = random(0.4, 1);
+  myRec.onResult = showResult;
+ mySpeech.setVolume(0.8);
 
-    // create the ball object and add it to the array
-    var myBall = new Ball(x, y, colorRandomizer, name);
-    balls.push(myBall);
-  }
-}
+ mySpeech.listVoices();
+mySpeech.setVoice(3);
+mySpeech.setRate(0.8);
+mySpeech.setPitch(1);
 
 
-
-function draw() {
-
-  background(0);
-  background(sfondo);
-  //imageMode(CENTER)
-  //image(sfondo, windowWidth/2, windowHeight/2, sfondo.width/2, sfondo.height/2);
+song.setVolume(0.3);
 
 
-  for (var j = 0; j < balls.length; j++) {
-
-		var randomCorrect = random(0.4, 1);
-    balls[j].display(randomCorrect);
-  }
 
 }
 
+function draw()
+{
 
+    ellipse(50, 50, 80, 80);
+}
 
-function Ball(_x, _y, _colorRandomizer, _name) {
+function activate() {
 
-  this.x = _x;
-  this.y = _y;
+}
 
+function showResult()
+{
 
-  this.display = function(_randomCorrect) {
+    background(192, 255, 192);
+if (myRec.onResult) {
+  textAlign(CENTER);
+    text(myRec.resultString, (width/2) - 300, (height/2) - 100 ,600,600)
+  }
 
-    var c = capture.get(noise(capture.width / 2), noise(capture.height / 2));
-    var bright = _colorRandomizer * lightness(c);
-    var col = color(255, 255, 0, bright * 5);
+}
 
-    textSize(20);
-    if (_name == "London") {
+function mousePressed() {
+if ( song.isPlaying() ) { // .isPlaying() returns a boolean
+    song.stop();
+    background(255,0,0);
+  } else {
 
-      console.log(_randomCorrect);
-			bright = _randomCorrect * bright + 30;
-	    col = color(255, 255, 0, bright * 10);
-      fill(col);
-      text(_name, this.x, this.y);
-    } else {
-      fill(col);
-      text(_name, this.x, this.y);
-    }
+    mySpeech.speak('Hey there! Where do you wanna go?');
+    myRec.start();
+    song.play();
+    background(0,255,0);
 
   }
 }
