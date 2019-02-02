5d59f0505d01a3e5a6c41887c122e0bb12141f0d
Lorenzo Ambrosini <lorenzo.ambrosini@mail.polimi.it>
2019-02-02 14:58:03 +0100

    Funzionante, posizionato giusto

--- 2_sketch.js
+++ 2_sketch.js
@@ -2,7 +2,7 @@ var capture;
 var sfondo;
 var data;
 
-var balls = [];
+var cities = [];
 
 function preload() {
   sfondo = loadImage("./assets/4/Departures.png");
@@ -20,15 +20,19 @@ function setup() {
   for (var i = 0; i < 14; i++) {
 
     // properties
-    var x = 120;
-    var y = i * 30 + 200;
+    var x = windowWidth/9;
+    var y = i * 35 + windowHeight/4;
     var name = data.people[i].name;
+    var time = data.people[i].time;
+    var code = data.people[i].code;
+    var platform = data.people[i].platform;
+    var status = data.people[i].status;
 
     var colorRandomizer = random(0.4, 1);
 
-    // create the ball object and add it to the array
-    var myBall = new Ball(x, y, colorRandomizer, name);
-    balls.push(myBall);
+    // create the City object and add it to the array
+    var myCity = new City(x, y, colorRandomizer, name, time, code, platform, status);
+    cities.push(myCity);
   }
 }
 
@@ -37,22 +41,21 @@ function setup() {
 function draw() {
 
   background(0);
-  background(sfondo);
   //imageMode(CENTER)
   //image(sfondo, windowWidth/2, windowHeight/2, sfondo.width/2, sfondo.height/2);
 
 
-  for (var j = 0; j < balls.length; j++) {
+  for (var j = 0; j < cities.length; j++) {
 
     var randomCorrect = random(0.4, 1);
-    balls[j].display(randomCorrect);
+    cities[j].display(randomCorrect);
   }
 
 }
 
 
 
-function Ball(_x, _y, _colorRandomizer, _name) {
+function City(_x, _y, _colorRandomizer, _name, _time, _code, _platform, _status) {
 
   this.x = _x;
   this.y = _y;
@@ -63,19 +66,30 @@ function Ball(_x, _y, _colorRandomizer, _name) {
     var c = capture.get(noise(capture.width / 2), noise(capture.height / 2));
     var bright = _colorRandomizer * lightness(c);
     var col = color(255, 255, 0, bright * 5);
+    var colLondon = color(255, 255, 0, bright * 5 + 60);
 
-    textSize(20);
+    textFont('led');
+    textSize(17);
+
     if (_name == "London") {
 
       //console.log(_randomCorrect);
-      bright = _randomCorrect * bright + 30;
-      col = color(255, 255, 0, bright * 10);
-      fill(col);
+      bright = bright + 30;
+      colLondon = color(255, 255, 0, bright * 5);
+      fill(colLondon);
       text(_name, this.x, this.y);
+      text(_time, this.x + 350, this.y);
+      text(_code, this.x + 600, this.y);
+      text(_platform, this.x + 725, this.y);
+      text(_status, this.x + 800, this.y);
+
     } else {
       fill(col);
-      textFont('led')
       text(_name, this.x, this.y);
+      text(_time, this.x + 350, this.y);
+      text(_code, this.x + 600, this.y);
+      text(_platform, this.x + 725, this.y);
+      text(_status, this.x + 800, this.y);
     }
 
   }
